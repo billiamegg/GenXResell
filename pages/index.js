df2a589068524ddb5422f302f4285671c0bdde0a
billiamegg <122169777+billiamegg@users.noreply.github.com>
2023-01-09 17:26:17 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,18 +15,7 @@ export default function Home() {
         <p className="description">
           This website is bieng hosted by ryken kennedy.  i thought it would be fun to start some sort of group website and so I did.
         </p>
-        <iframe src="https://tawk.to/chat/63bc9d8747425128790c8d39/1gmccelvi" />
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/63bc9d8747425128790c8d39/1gmccelvi';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
+        <iframe src="https://tawk.to/chat/63bc9d8747425128790c8d39/1gmccelvi" width="100px" height="400px" />
       </main>
 
       <Footer />
