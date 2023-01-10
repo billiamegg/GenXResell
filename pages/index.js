c456ad8fdffa91a6bd852b232792047c957cd2b4
billiamegg <122169777+billiamegg@users.noreply.github.com>
2023-01-10 15:50:54 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,25 @@ export default function Home() {
           This website is bieng hosted by ryken kennedy.  i thought it would be fun to start some sort of group website and so I did.
           contact billiam_egg@yahoo.com on more info on how to become a member lol.
         </p>
+    <div id="disqus_thread"></div>
+<script>
+    /**
+    *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+    *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
+    /*
+    var disqus_config = function () {
+    this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+    this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    };
+    */
+    (function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = 'https://voicecrackerclub.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
       </main>
 
       <Footer />
