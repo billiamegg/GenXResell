ec22dc2c0e37f2cd047e5249b23d5be50a90a7f3
billiamegg <122169777+billiamegg@users.noreply.github.com>
2023-01-10 15:56:43 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -35,6 +35,7 @@ export default function Home() {
     })();
 </script>
 <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+  <script id="dsq-count-scr" src="//voicecrackerclub.disqus.com/count.js" async></script>
       </main>
 
       <Footer />
